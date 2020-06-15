b697bd1c91452e29f09ffc9e1965f4361bfc2809
mathru <support@mathru.net>
2020-06-16 01:58:06 +0900

    Format

--- lib/widget/uiloginform.dart
+++ lib/widget/uiloginform.dart
@@ -27,55 +27,55 @@ class UILoginForm extends StatelessWidget with UIFormMixin {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: this.formKey,
-      autovalidate: true,
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextFormField(
-                maxLength: 200,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: "Please enter a email address".localize(),
-                  labelText: "Email".localize(),
-                ),
-                autovalidate: false,
-                validator: (value) {
-                  if (isEmpty(value))
-                    return "Please enter some text".localize();
-                  return null;
-                },
-                onSaved: (value) {
-                  if (isEmpty(value)) return;
-                  context.form["email"] = value;
-                })),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextFormField(
-                obscureText: true,
-                maxLength: 50,
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: "Please enter a password".localize(),
-                  labelText: "Password".localize(),
-                ),
-                autovalidate: false,
-                validator: (value) {
-                  if (isEmpty(value))
-                    return "Please enter some text".localize();
-                  if (value.length < 8) {
-                    return Texts.format(
-                        "Please enter at least %s characters.".localize(),
-                        [8]);
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  if (isEmpty(value)) return;
-                  context.form["password"] = value;
-                }))
-      ]));
+        key: this.formKey,
+        autovalidate: true,
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextFormField(
+                  maxLength: 200,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: "Please enter a email address".localize(),
+                    labelText: "Email".localize(),
+                  ),
+                  autovalidate: false,
+                  validator: (value) {
+                    if (isEmpty(value))
+                      return "Please enter some text".localize();
+                    return null;
+                  },
+                  onSaved: (value) {
+                    if (isEmpty(value)) return;
+                    context.form["email"] = value;
+                  })),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextFormField(
+                  obscureText: true,
+                  maxLength: 50,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: "Please enter a password".localize(),
+                    labelText: "Password".localize(),
+                  ),
+                  autovalidate: false,
+                  validator: (value) {
+                    if (isEmpty(value))
+                      return "Please enter some text".localize();
+                    if (value.length < 8) {
+                      return Texts.format(
+                          "Please enter at least %s characters.".localize(),
+                          [8]);
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    if (isEmpty(value)) return;
+                    context.form["password"] = value;
+                  }))
+        ]));
   }
 }
