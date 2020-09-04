e790d0e9f4b88387e55ce1fbba75309e956f975a
mathru <user@mathru.net>
2020-09-04 19:07:22 +0900

    Formの変更に伴う変更

--- lib/widget/changeemail.dart
+++ lib/widget/changeemail.dart
@@ -1,63 +1,12 @@
 part of masamune.firebase;
 
-class ChangeEmail extends UIPageForm {
-  /// Page title.
-  @protected
-  String get title => "Change Email".localize();
-
-  /// Set the form type.
-  ///
-  /// Available for login and password reset page.
-  FormBuilderType get formType => FormBuilderType.center;
-
-  /// Creating a app bar.
-  ///
-  /// [context]: Build context.
-  @protected
-  @override
-  Widget appBar(BuildContext context) {
-    return AppBar(title: Text(this.title));
-  }
-
+class ChangeEmail extends UIPageChangeEmail {
   /// Initial definition of the controller.
   ///
   /// [context]: Build context.
   @override
   Map<String, String> define(BuildContext context) {
     return {"email": FirestoreAuth.getEmail()};
-  }
-
-  /// Form body definition.
-  ///
-  /// [context]: Build context.
-  @override
-  List<Widget> formBody(BuildContext context,
-      Map<String, TextEditingController> controller, IDataDocument form) {
-    return [
-      Text(
-        "Please enter the information you want to change".localize(),
-        textAlign: TextAlign.center,
-      ),
-      SizedBox(height: 20),
-      FormItemTextField(
-        controller: controller["email"],
-        hintText: "Please enter a email address".localize(),
-        labelText: "Email".localize(),
-        keyboardType: TextInputType.emailAddress,
-        onSaved: (value) {
-          if (isEmpty(value)) return;
-          form["email"] = value;
-        },
-      ),
-    ];
-  }
-
-  /// Data load.
-  ///
-  /// [context]: Build context.
-  @override
-  FutureOr<IDataDocument<IDataField>> loader(BuildContext context) {
-    return null;
   }
 
   /// What happens when a form is submitted.
